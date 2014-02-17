4f943665327e58613454c805eae0a798ca9b50e8
Terry Moore <terry.moore.ii@gmail.com>
2014-02-16 20:41:54 -0500

    Added an add feed/category directive and updated scroll bar css

--- app/js/directives.js
+++ app/js/directives.js
@@ -15,4 +15,31 @@ angular.module('myApp.directives', [])
 			});
 		}
 	}
+})
+.directive('tlmiiAdd', function(){
+	return {
+		restrict: 'AEC',
+		link: function(scope, element, attrs){
+			var $self = $(element);
+			var $form = $self.next('form');
+
+			$(element).on('click', function(){
+				var $this = $(this);
+				//$this.hide();
+				$form.show();
+				
+			});
+
+			$(document).on('mouseup', function(e){
+			    var container = $form;
+
+			    if (!container.is(e.target)
+			        && container.has(e.target).length === 0){
+			        $self.show();
+			        $form.hide();
+			    }
+			});
+		}
+	}
 })	
+
